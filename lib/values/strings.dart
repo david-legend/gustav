25320de644cea34b6e66e6d1d2e675778088cb3f
David Cobbina <davidcobbina47@gmail.com>
2022-07-17 14:18:06 +0000

    first commit

---
+++ lib/values/strings.dart
@@ -0,0 +1,173 @@
+part of values;
+
+class StringConst {
+  //strings
+  static const String APP_NAME = "Jade Macy Portfolio";
+
+  //Nav Section
+  static const String NAME_ABBREV = "TA";
+  static const String HOME = "Home";
+  static const String ABOUT = "About";
+  static const String RESUME = "Resume";
+  static const String SKILLS_AND_CERTIFICATION = "Skills & Certifications";
+  static const String CONTACT = "Contact Me";
+
+  //Header Section
+  static const String INTRO = "Hello! I'm";
+
+//  static const String NAME = "James Martin";
+  static const String NAME = "Theodore Andoh";
+  static const String PROFESSIONAL_POSITION =
+      "Solar PV Developer";
+  static const String CATCH_LINE = "Building the Future of Solar with you";
+  static const String EMAIL = "davidcobbina47@gmail.com";
+  static const String EMAIL_URL = "mailto:<$EMAIL>?subject=&body=";
+
+  //About Section
+  static const String ABOUT_ME = "About Me";
+  static const String PROFESSIONAL = "Professional";
+  static const String SUMMARY_TITLE = "Summary";
+  static const String ABOUT_ME_SUMMARY_1 =
+      '''I am a hard working and self motivated in the things I do. I am into renewable energies to be specific solar system design and Installation. I am also into Renewable technologies that aims to reduce emission of GHG''';
+  // static const String ABOUT_ME_SUMMARY_2 =
+  //     "I am also into Renewable technologies that aims to reduce emission of GHG";
+  // static const String ABOUT_ME_SUMMARY_3 =
+  //     "I am also into Renewable technologies that aims to reduce emission of GHG";
+  // static const String ABOUT_ME_SUMMARY_4 =
+  //     "I have learned and honed valuable collaboration and team management skills that have allowed me to both";
+  // static const String ABOUT_ME_SUMMARY_5 =
+  //     "manage and work constructively and inventively in group-based environment.";
+
+  //Education Section
+  static const String MY = "My";
+
+  static const String EDUCATION = "Education";
+
+  //Education Section Data
+  static const String EDUCATION_CURRENT_MONTH_YEAR = "June 2021";
+  static const String EDUCATION_STARTED_MONTH_YEAR = "August 2014";
+  static const String WORK_CURRENT_MONTH_YEAR = "June 2018";
+  static const String WORK_STARTED_MONTH_YEAR = "Sept 2020";
+
+  static const String EDUCATION_1 = "Msc. Information Technology";
+  static const String EDUCATION_1_URL = "https://www.cmu.edu/";
+
+//  static const String POSITION_1 = "Software Engineer Intern";
+  static const String EDUCATION_1_DESC =
+      "Majoring in applied machine learning and Data science.";
+  static const String LOCATION_1 = "Carnegie Mellon University, Pit, USA";
+  static const String DURATION_1 = "Sept. 2020 - Dec. 2021";
+
+  static const String EDUCATION_2 = "Bsc. Agricultural Engineering";
+  static const String EDUCATION_2_URL = "https://www.knust.edu.gh/";
+  static const String EDUCATION_2_DESC =
+      "Majored in Agricultural & Biosystems Engineering";
+  static const String LOCATION_2 = "Kwame Nkrumah University Of Science & Technology, Ghana";
+  static const String DURATION_2 = "September 2014 - May 2018";
+
+  static const String WORK_1 = "Freelance";
+  static const String WORK_1_URL = "";
+  static const String WORK_1_POSITION = "Solar Pvsyst Design";
+  static const String WORK_1_ROLE_1 =
+      "PvSyst Design for homes in netherlands";
+  static const String WORK_1_ROLE_2 =
+      "Shading analysis of parks and building with pvsyst";
+  static const String WORK_1_ROLE_3 =
+      "Solar Photovoltaic installation";
+  static const String WORK_1_LOCATION = "Cottbus, Germany";
+  static const String WORK_1_DURATION = "January 2022 - May 2022";
+
+
+  static const String WORK_2 = "Freelance";
+  static const String WORK_2_URL = "";
+  static const String WORK_2_POSITION = "Automation of Devices";
+  static const String WORK_2_ROLE_1 =
+      "Building and automating feeding systems for Animals in Agricultural farms in Ghana";
+  static const String WORK_2_ROLE_2 =
+      "Interfaced with clients on a weekly basis, providing technological expertise";
+  static const String WORK_2_ROLE_3 =
+      "Modified existing software to correct errors, upgrade interfaces and improve performance";
+  static const String WORK_2_LOCATION = "Ghana";
+  static const String WORK_2_DURATION = "January 2020 - December 2020";
+
+  static const String WORK_3 = "Nestlé";
+  static const String WORK_3_URL = "https://www.nestle-cwa.com/en";
+  static const String WORK_3_POSITION = "Quality Assurance ";
+  static const String WORK_3_ROLE_1 =
+      "Developed and shipped highly interactive web applications for Apple Music using Ember.js";
+  static const String WORK_3_ROLE_2 =
+      "Automating pumps for Individual clients";
+  static const String WORK_3_ROLE_3 =
+      "Installation of tracking devices for Automotive";
+  static const String WORK_3_LOCATION = "Tema-Industrial Area,Ghana";
+  static const String WORK_3_DURATION = "October 2018 - September 2019";
+
+  static const String WORK_4 = "Logs and Lumber Limited-Intern";
+  static const String WORK_4_URL = "";
+  static const String WORK_4_POSITION = "Junior Plant Engineer ";
+  static const String WORK_4_ROLE_1 =
+      "Worked on hydraulic press and Kiln Dryers";
+  static const String WORK_4_ROLE_2 =
+      "Worked on cyclones for saw dust extraction";
+  static const String WORK_4_ROLE_3 =
+      "Worked On Overhead Crane and general maintenance";
+  static const String WORK_4_LOCATION = "Kumasi, Ghana";
+  static const String WORK_4_DURATION = "May 2017 - August 2017";
+
+  //Experience Section
+  static const String EXPERIENCE = "Experience";
+
+  //Skills Section
+  static const String MY_CODING = "My Coding";
+  static const String SKILLS = "Skills";
+  static const String CERTIFICATIONS = "Certifications";
+
+  static const String PYTHON = "Python";
+  static const String JAVA = "Java";
+  static const String C_PLUS_PLUS = "C++";
+  static const String JAVASCRIPT = "Javascript";
+  static const String HTML = "HTML";
+  static const String CSS = "CSS";
+
+  static const IOT_CERT = "IOT Certification";
+  static const RISA = "RISA";
+  static const ASSOCIATE_ANDROID_DEV_CERT = "Associate Android Developer";
+  static const GOOGLE = "Google";
+  static const DATA_SCIENCE_CERT = "Data Science";
+  static const UDACITY = "Udacity";
+
+  //Portfolio Section
+  static const String PORTFOLIO = "Portfolio";
+  static const String GRAPHICS = "Graphics";
+  static const String APP_DESIGN = "App Design";
+  static const String BRANDING = "Branding";
+  static const String GAME_DESIGN = "Game Design";
+  static const String WEB_DESIGN = "Web Design";
+  static const String ALL = "All";
+  static const String DIGITAL_WALL_POSTER = "Digital Wall Poster";
+  static const String SPACE_STORY = "Space Story";
+  static const String STREET_ART = "Street Art";
+  static const String SOUL_TEARS = "Soul Tears";
+  static const String MURAL_PRIDE = "Mural Pride";
+  static const String ABYSS = "Abyss";
+  static const String AURORA_PISTON = "Aurora Piston";
+  static const String FREESTYLE = "Freestyle";
+
+  //Footer Section
+  static const String RIGHTS_RESERVED = "© 2021 All rights reserved";
+  static const String BUILT_BY = "Built with Flutter by";
+  static const String DAVID_COBBINA = "David Cobbina";
+  static const String DESIGNED_BY = "Designed by";
+  static const String ADEEL_RAZA = "Adeel Raza";
+  static const String MADE_IN_GHANA = "Made in";
+  static const String WITH_LOVE = "with";
+  static const String FIND_ME = "Find me on..";
+
+  //URL Links
+  static const String LINKED_IN_URL = "https:linkedin.com/in/";
+  static const String GITHUB_URL = "https://github.com/";
+  static const String TWITTER_URL = "http://twitter.com/";
+  static const String DAVID_LEGEND_URL = "https://davidcobbina.com/#/";
+  static const String DESIGN_URL =
+      "https://www.behance.net/gallery/95351691/Resume-Design-03";
+}
